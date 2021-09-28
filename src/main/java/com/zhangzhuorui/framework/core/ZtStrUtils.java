bd3921292f38e375db6c42921e4a667acbd0c5ed
zhangtao <17091648421@126.com>
2021-09-28 13:08:16 +0800

    乐观锁字段名

--- src/main/java/com/zhangzhuorui/framework/core/ZtStrUtils.java
+++ src/main/java/com/zhangzhuorui/framework/core/ZtStrUtils.java
@@ -12,42 +12,44 @@ package com.zhangzhuorui.framework.core;
 public class ZtStrUtils {
 
     //region 返回的状态码
-    public static final String CODE = "code" ;
-    public static final String EMPTY = "empty" ;
-    public static final String TOTAL = "total" ;
-    public static final String MSG = "message" ;
-    public static final String DATA = "data" ;
-    public static final String SUCCESS = "success" ;
-    public static final String SUCCESS_CODE = "OK" ;
-    public static final String SUCCESS_MSG = "操作成功" ;
-    public static final String FAIL_CODE = "FAIL" ;
-    public static final String FAIL_MSG = "操作失败，请刷新页面，获取最新数据后重试" ;
+    public static final String CODE = "code";
+    public static final String EMPTY = "empty";
+    public static final String TOTAL = "total";
+    public static final String MSG = "message";
+    public static final String DATA = "data";
+    public static final String SUCCESS = "success";
+    public static final String SUCCESS_CODE = "OK";
+    public static final String SUCCESS_MSG = "操作成功";
+    public static final String FAIL_CODE = "FAIL";
+    public static final String FAIL_MSG = "操作失败，请刷新页面，获取最新数据后重试";
     //endregion
 
-    public static final String ID_FIELD_NAME = "id" ;
-    public static final String ID_COLUMN_NAME = "id" ;
+    public static final String ID_FIELD_NAME = "id";
+    public static final String ID_COLUMN_NAME = "id";
+    public static final String VERSION_FIELD_NAME = "version";
+    public static final String VERSION_COLUMN_NAME = "version";
 
     //region 增删改查api
-    public static final String DELEMITER = "/" ;
-    public static final String GET_ENUM_NAME = DELEMITER + "getEnumName" ;
-    public static final String GET_ENUM_INFO = DELEMITER + "getEnumInfo" ;
-    public static final String SELECT_SIMPLE = DELEMITER + "selectSimple" ;
-    public static final String SELECT_SIMPLE_ALL = DELEMITER + "selectSimpleAll" ;
-    public static final String SELECT_ID_SIMPLE = DELEMITER + "selectIdSimple" ;
-    public static final String INSERT_SIMPLE = DELEMITER + "insertSimple" ;
-    public static final String UPDATE_SIMPLE = DELEMITER + "updateSimple" ;
-    public static final String DELETE_SIMPLE = DELEMITER + "deleteSimple" ;
-    public static final String INSERT_BATCH_SIMPLE = DELEMITER + "insertBatchSimple" ;
-    public static final String DELETE_BATCH_SIMPLE = DELEMITER + "deleteBatchSimple" ;
+    public static final String DELEMITER = "/";
+    public static final String GET_ENUM_NAME = DELEMITER + "getEnumName";
+    public static final String GET_ENUM_INFO = DELEMITER + "getEnumInfo";
+    public static final String SELECT_SIMPLE = DELEMITER + "selectSimple";
+    public static final String SELECT_SIMPLE_ALL = DELEMITER + "selectSimpleAll";
+    public static final String SELECT_ID_SIMPLE = DELEMITER + "selectIdSimple";
+    public static final String INSERT_SIMPLE = DELEMITER + "insertSimple";
+    public static final String UPDATE_SIMPLE = DELEMITER + "updateSimple";
+    public static final String DELETE_SIMPLE = DELEMITER + "deleteSimple";
+    public static final String INSERT_BATCH_SIMPLE = DELEMITER + "insertBatchSimple";
+    public static final String DELETE_BATCH_SIMPLE = DELEMITER + "deleteBatchSimple";
 
-    public static final String REFRESH_CACHE = DELEMITER + "refresh" ;
-    public static final String REFRESH_CACHE_BY_NAME = DELEMITER + "refreshByName" ;
+    public static final String REFRESH_CACHE = DELEMITER + "refresh";
+    public static final String REFRESH_CACHE_BY_NAME = DELEMITER + "refreshByName";
     //endregion
 
     //默认分页信息
-    public static final String START = "1" ;//start  current
-    public static final String LIMIT = "20" ;//limit  size
-    public static final String MAX_PAGE = "1000" ;//默认最大每页1000条
+    public static final String START = "1";//start  current
+    public static final String LIMIT = "20";//limit  size
+    public static final String MAX_PAGE = "1000";//默认最大每页1000条
     public static final Integer NO_COUNT = -1;
 
 }
