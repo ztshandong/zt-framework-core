48e5e6c2f3705d9baee1e90c57c44d46308d215b
zhangtao <17091648421@126.com>
2022-10-13 16:35:33 +0800

    高级排序

--- src/main/java/com/zhangzhuorui/framework/core/ZtUtils.java
+++ src/main/java/com/zhangzhuorui/framework/core/ZtUtils.java
@@ -4,15 +4,20 @@ import com.alibaba.fastjson.JSON;
 import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Field;
+import java.text.Collator;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author :  张涛 zhangtao
@@ -24,6 +29,72 @@ import java.util.Optional;
  * @updateRemark :
  */
 public class ZtUtils {
+
+    public final static Comparator<Object> COMPARATOR = Collator.getInstance(Locale.CHINESE);
+    final static String patterNumStr = "^\\d*";
+    final static String patterNaNStr = "^\\D*";
+
+    final static Pattern patterNum = Pattern.compile(patterNumStr);
+    final static Pattern patterNan = Pattern.compile(patterNaNStr);
+
+    public static int comparePlus(String str1, String str2) {
+        if (Objects.equals(str1, str2)) {
+            return 0;
+        }
+        if (StringUtils.isEmpty(str1)) {
+            return -1;
+        } else if (StringUtils.isEmpty(str2)) {
+            return 1;
+        }
+        int int1 = -1;
+        int int2 = -1;
+
+        Matcher matchNum1 = patterNum.matcher(str1);
+        if (matchNum1.find()) {
+            String group1 = matchNum1.group();
+            if (!StringUtils.isEmpty(group1)) {
+                int1 = Integer.parseInt(group1);
+            }
+        }
+        Matcher matchNum2 = patterNum.matcher(str2);
+        if (matchNum2.find()) {
+            String group2 = matchNum2.group();
+            if (!StringUtils.isEmpty(group2)) {
+                int2 = Integer.parseInt(group2);
+            }
+        }
+
+        // 数字优先
+        if ((int1 > 0) && (int2 == -1)) {
+            return -1;
+        }
+        if ((int2 > 0) && (int1 == -1)) {
+            return 1;
+        }
+        if (int1 == int2) {
+            str1 = str1.substring(String.valueOf(int1).length());
+            str2 = str2.substring(String.valueOf(int2).length());
+            String s1 = "";
+            String s2 = "";
+            Matcher matchNan1 = patterNan.matcher(str1);
+            Matcher matchNan2 = patterNan.matcher(str2);
+            if (matchNan1.find()) {
+                s1 = matchNan1.group();
+            }
+            if (matchNan2.find()) {
+                s2 = matchNan2.group();
+            }
+            if (!Objects.equals(s1, s2)) {
+                return COMPARATOR.compare(s1, s2);
+            } else {
+                str1 = str1.substring(s1.length());
+                str2 = str2.substring(s2.length());
+                return comparePlus(str1, str2);
+            }
+        }
+        return int1 - int2;
+
+    }
 
     public static final DateTimeFormatter SHORT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.CHINA);
     public static final DateTimeFormatter LONG_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
